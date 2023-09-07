1b6617feb2e9546de16061159b3418187ec574c1
MiguelM01 <miguel.morales@fintecheando.mx>
2023-09-06 23:18:18 -0600

    PROYECTO EN BLANCO

---
+++ src/main/java/com/demo/elkexample/ElkExampleApplication.java
@@ -0,0 +1,13 @@
+package com.demo.elkexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElkExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElkExampleApplication.class, args);
+	}
+
+}
